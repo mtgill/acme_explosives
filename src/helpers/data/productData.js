661739e39064301bd58df17a84083e12316f5a80
Matt Gill <mtgill90@outlook.com>
2019-05-25 09:01:29 -0500

    wip

--- src/helpers/data/productData.js
+++ src/helpers/data/productData.js
@@ -9,8 +9,16 @@ const loadProductsForTypes = categoriesWithTypes => new Promise((resolve, reject
       const allProducts = Object.values(resp.data.products[0]);
       for (let i = 0; i < categoriesWithTypes.length; i += 1) {
         for (let j = 0; j < categoriesWithTypes[i].types.length; j += 1) {
+          // let newObject = {};
+          // const type = categoriesWithTypes[i].types[j].id;
+          // const category = categoriesWithTypes[i].id;
+          // newObject = {
+          //   type,
+          //   category,
+          // };
+          // typeArray.push(newObject);
           typeArray.push(categoriesWithTypes[i].types[j].id);
-          typeCategories.push(categoriesWithTypes[i].types[j].category)
+          // typeCategories.push(categoriesWithTypes[i].types[j].category);
           // console.error(`please god work ${categoriesWithTypes[i].types[j].id}`);
         }
       }
@@ -28,7 +36,7 @@ const loadProductsForTypes = categoriesWithTypes => new Promise((resolve, reject
         const newProduct = product;
         const matchingTypes = typeArray.filter(type => type.id === product.type);
         newProduct.type = matchingTypes;
-        console.error(`matching products ${matchingTypes}`);
+        // console.error(`matching products ${matchingTypes}`);
         return newProduct;
       });
       // console.error(`types with products${typesWithProducts}`);
