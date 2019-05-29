45a6ce286b031bf70eee5e912168b9493b99984d
Matt Gill <mtgill90@outlook.com>
2019-05-29 17:20:43 -0500

    bonus works

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -5,12 +5,8 @@ import util from '../../helpers/util';
 
 const allButton = document.getElementById('all');
 const groundButton = document.getElementById('ground');
-// const aerialButton = document.getElementById('aerial');
-
-let sortArray = [];
-const setSortArray = (array) => {
-  sortArray = array;
-};
+const aerialButton = document.getElementById('aerial');
+const dropdown = document.getElementById('dropdown');
 
 const writeProducts = (products) => {
   let domString = '';
@@ -44,11 +40,29 @@ const writeProducts = (products) => {
       }
       util.printToDom('products', domString);
     });
+  } else if (aerialButton.checked === true) {
+    products.forEach((product) => {
+      if (product.categoryId === aerialButton.id) {
+        domString += '<div class="col-3">';
+        domString += `<div id='${product.name}' class="card p-2">`;
+        domString += '<div class="card-body">';
+        domString += `<h5 class="card-title">${product.name}</h5>`;
+        domString += `<h6>Type: ${product.type}</h6>`;
+        domString += `<h6>Category: ${product.category}</h6>`;
+        domString += `<h6>Description: ${product.description}</h6>`;
+        domString += '</div>';
+        domString += '</div>';
+        domString += '</div>';
+      }
+      util.printToDom('products', domString);
+    });
   }
 };
 
 const buttonEvents = (array) => {
-  groundButton.addEventListener('select', writeProducts(array));
+  dropdown.addEventListener('change', () => {
+    writeProducts(array);
+  });
 };
 
 const initProducts = () => {
@@ -58,7 +72,6 @@ const initProducts = () => {
       productData.loadProductsForTypes(categoriesWithTypes)
         .then((productArray) => {
           writeProducts(productArray);
-          setSortArray(productArray);
           buttonEvents(productArray);
         });
     })
